cb7e9dcb43aabc7214919cfc276874bee9c367a6
zq <995521405@qq.com>
2021-09-02 22:04:25 +0800

    Initial commit

---
+++ src/main/java/com/jack/spring5/Spring5Application.java
@@ -0,0 +1,13 @@
+package com.jack.spring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5Application.class, args);
+    }
+
+}
